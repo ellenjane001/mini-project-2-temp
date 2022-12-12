2a4318df029941abf963738a557798f63c12b5a4
ellenjane001 <ellenjane.000001998@gmail.com>
2022-12-12 17:05:59 +0800

    added remove item in cart function

--- public/js/main.js
+++ public/js/main.js
@@ -127,7 +127,13 @@ let Cart = {
     LOAD: () => {
         let cartQuantity
         if (Object.keys(sessionStorage).includes('cart')) {
-            cartQuantity = JSON.parse(sessionStorage.getItem('cart')).items.map(e => e.count).reduce((a, b) => a + b)
+            let cart = JSON.parse(sessionStorage.getItem('cart')).items.map(e => e.count)
+            if (cart.length > 0) {
+                cart.reduce((a, b) => a + b)
+                cartQuantity = cart
+            } else {
+                cartQuantity = 0
+            }
         } else {
             cartQuantity = 0
         }
@@ -135,80 +141,97 @@ let Cart = {
     },
     VIEW: () => {
         if (Object.keys(sessionStorage).includes('cart')) {
-            if (document.querySelector('#cart-modal .modal-body table tbody').children.length > 0) {
-                document.querySelector('#cart-modal .modal-body table tbody').innerHTML = ''
+            Cart.clear()
+            if (JSON.parse(sessionStorage.getItem('cart')).items.length > 0) {
+                let data = JSON.parse(sessionStorage.getItem('cart'))
+                for (let i = 0; i < data.items.length; i++) {
+                    let elementsObj = {
+                        tr: document.createElement('tr'), tdCheck: document.createElement('td'), input: document.createElement('input'), tdName: document.createElement('td'), thumbnail: document.createElement('img'), tdImage: document.createElement('td'), tdQuantity: document.createElement('td'), tdAmount: document.createElement('td'), tdDelete: document.createElement('td'), tdDeleteOne: document.createElement('td')
+                    }
+                    const { tr, tdCheck, input, tdName, thumbnail, tdImage, tdQuantity, tdAmount, tdDelete, tdDeleteOne } = elementsObj
+                    tr.style.verticalAlign = 'middle'
+                    input.type = 'checkbox'
+                    input.setAttribute('data-value', JSON.stringify(data.items[i]))
+
+                    thumbnail.height = 100
+                    thumbnail.width = 100
+                    thumbnail.style.objectFit = 'contain'
+                    thumbnail.src = data.items[i].image
+                    tdAmount.innerText = `$${data.items[i].price.toFixed(2)}`
+                    tdName.innerText = data.items[i].name
+                    tdQuantity.innerText = data.items[i].count
+
+                    tdDelete.classList.add('text-center')
+                    tdDeleteOne.classList.add('text-center')
+                    let icon = document.createElement('i')
+                    let iconClasses = ['fa-solid', 'fa-trash', 'fa-xl']
+                    for (classes in iconClasses) {
+                        icon.classList.add(iconClasses[classes])
+                    }
+                    let icon2 = document.createElement('i')
+                    let icon2Classes = ['fa-solid', 'fa-minus', 'fa-xl']
+                    for (classes in icon2Classes) {
+                        icon2.classList.add(icon2Classes[classes])
+                    }
+                    tdDeleteOne.setAttribute('data-value', JSON.stringify(data.items[i]))
+                    tdDelete.setAttribute('data-value', JSON.stringify(data.items[i]))
+                    tdDeleteOne.addEventListener('click', Cart.REMOVE)
+                    tdDelete.addEventListener('click', Cart.DELETE)
+                    tdDeleteOne.style.cursor = 'pointer'
+                    tdDelete.style.cursor = 'pointer'
+                    tdCheck.appendChild(input)
+                    tdImage.appendChild(thumbnail)
+                    tdDeleteOne.appendChild(icon2)
+                    tdDelete.appendChild(icon)
+
+                    tdQuantity.classList.add('text-center')
+                    tdQuantity.classList.add('h3')
+                    let elements = [tdCheck, tdName, tdImage, tdQuantity, tdAmount, tdDeleteOne, tdDelete]
+                    for (let a = 0; a < elements.length; a++) {
+                        tr.appendChild(elements[a])
+                    }
+                    console.log(tr)
+                    document.querySelector('#cart-modal .modal-body table tbody').appendChild(tr)
+                }
+                document.getElementById('total-amount').innerText = `$${data.totalAmount.toFixed(2)}`
             }
-            let data = JSON.parse(sessionStorage.getItem('cart'))
-            for (let i = 0; i < data.items.length; i++) {
-                let elementsObj = {
-                    tr: document.createElement('tr'), tdCheck: document.createElement('td'), input: document.createElement('input'), tdName: document.createElement('td'), thumbnail: document.createElement('img'), tdImage: document.createElement('td'), tdQuantity: document.createElement('td'), tdAmount: document.createElement('td'), tdDelete: document.createElement('td'), tdDeleteOne: document.createElement('td')
-                }
-                const { tr, tdCheck, input, tdName, thumbnail, tdImage, tdQuantity, tdAmount, tdDelete, tdDeleteOne } = elementsObj
-                tr.style.verticalAlign = 'middle'
-                input.type = 'checkbox'
-                input.setAttribute('data-value', JSON.stringify(data.items[i]))
-
-                thumbnail.height = 100
-                thumbnail.width = 100
-                thumbnail.style.objectFit = 'contain'
-                thumbnail.src = data.items[i].image
-                tdAmount.innerText = `$${data.items[i].price}`
-                tdName.innerText = data.items[i].name
-                tdQuantity.innerText = data.items[i].count
-
-                tdDelete.classList.add('text-center')
-                tdDeleteOne.classList.add('text-center')
-                let icon = document.createElement('i')
-                let iconClasses = ['fa-solid', 'fa-trash', 'fa-xl']
-                for (classes in iconClasses) {
-                    icon.classList.add(iconClasses[classes])
-                }
-                let icon2 = document.createElement('i')
-                let icon2Classes = ['fa-solid', 'fa-minus', 'fa-xl']
-                for (classes in icon2Classes) {
-                    icon2.classList.add(icon2Classes[classes])
-                }
-                tdDeleteOne.setAttribute('data-value', JSON.stringify(data.items[i]))
-                tdDelete.setAttribute('data-value', JSON.stringify(data.items[i]))
-                tdDeleteOne.addEventListener('click', Cart.REMOVE)
-                tdDelete.addEventListener('click', Cart.DELETE)
-                tdDeleteOne.style.cursor = 'pointer'
-                tdDelete.style.cursor = 'pointer'
-                tdCheck.appendChild(input)
-                tdImage.appendChild(thumbnail)
-                tdDeleteOne.appendChild(icon2)
-                tdDelete.appendChild(icon)
-
-                tdQuantity.classList.add('text-center')
-                tdQuantity.classList.add('h3')
-                let elements = [tdCheck, tdName, tdImage, tdQuantity, tdAmount, tdDeleteOne, tdDelete]
-                for (let a = 0; a < elements.length; a++) {
-                    tr.appendChild(elements[a])
-                }
-                console.log(tr)
-                document.querySelector('#cart-modal .modal-body table tbody').appendChild(tr)
+            else {
+                Cart.noItem()
             }
-            document.getElementById('total-amount').innerText = `$${data.totalAmount.toFixed(2)}`
+
         } else {
-            let tr = document.createElement('tr')
-            let td = document.createElement('td')
-            td.innerText = 'cart is empty'
-            tr.appendChild(td)
-            td.colSpan = document.querySelector('#cart-modal .modal-body table thead tr').children.length
-            td.classList.add('text-center')
-            td.classList.add('fst-italic')
-            td.classList.add('fw-bold')
-            document.querySelector('#cart-modal .modal-body table tbody').appendChild(tr)
+            Cart.noItem()
         }
     },
     DELETE: (e) => {
         let data = JSON.parse(e.target.getAttribute('data-value'))
         let value = Cart.initiator()
-        
-        console.log(value.items.map(e => e.name == data.name))
+        console.log(data)
+
+        value.items = value.items.filter(a => a.name !== data.name)
+        console.log(value)
+        Cart.clear()
+        Cart.noItem()
+        Cart.setStorage(value)
     },
     REMOVE: (e) => {
         let data = JSON.parse(e.target.getAttribute('data-value'))
+    },
+    noItem: () => {
+        let tr = document.createElement('tr')
+        let td = document.createElement('td')
+        td.innerText = 'cart is empty'
+        tr.appendChild(td)
+        td.colSpan = document.querySelector('#cart-modal .modal-body table thead tr').children.length
+        td.classList.add('text-center')
+        td.classList.add('fst-italic')
+        td.classList.add('fw-bold')
+        document.querySelector('#cart-modal .modal-body table tbody').appendChild(tr)
+    },
+    clear: () => {
+        if (document.querySelector('#cart-modal .modal-body table tbody').children.length > 0) {
+            document.querySelector('#cart-modal .modal-body table tbody').innerHTML = ''
+        }
     }
 }
 document.getElementById('cart-btn').addEventListener('click', e => {
